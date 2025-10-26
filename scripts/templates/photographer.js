020ccade64fe0aa38f7a246cf12c25067a296676
Leo-Brd <leobordet.pro@gmail.com>
2025-10-26 18:16:25 +0100

    fix(main page): accessibility is better with aria labels and tabindex

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -4,12 +4,14 @@ function photographerTemplate(data) {
 
     function getUserCardDOM() {
         const article = document.createElement('article');
+        article.setAttribute('role', 'listitem');
 
         // Create link to photographer's page
         const link = document.createElement('a');
         link.setAttribute('href', `photographer.html?id=${id}`);
         link.setAttribute('class', 'photographer-link');
         link.setAttribute('aria-label', name);
+        link.setAttribute('tabindex', '0');
 
         const img = document.createElement('img');
         img.setAttribute('src', picture);
@@ -18,6 +20,7 @@ function photographerTemplate(data) {
 
         const h2 = document.createElement('h2');
         h2.textContent = name;
+        h2.setAttribute('tabindex', '-1');
         link.appendChild(h2);
 
         // Add link to card
@@ -27,18 +30,21 @@ function photographerTemplate(data) {
         const location = document.createElement('p');
         location.textContent = `${city}, ${country}`;
         location.className = 'photographer-location';
+        location.setAttribute('aria-label', `Localisation : ${city}, ${country}`);
         article.appendChild(location);
 
         // Tagline
         const taglineElem = document.createElement('p');
         taglineElem.textContent = tagline;
         taglineElem.className = 'photographer-tagline';
+        taglineElem.setAttribute('aria-label', `Slogan : ${tagline}`);
         article.appendChild(taglineElem);
 
         // Price per day
         const priceElem = document.createElement('p');
         priceElem.textContent = `${price}€/jour`;
         priceElem.className = 'photographer-price';
+        priceElem.setAttribute('aria-label', `Tarif : ${price} euros par jour`);
         article.appendChild(priceElem);
 
         return article;
